d153fdb5df81a08db2ccbec00114fcd110cd33ab
YOHA1505 <you@example.com>
2024-05-14 14:05:57 +0200

    Första delen

--- src/NetworkingServer.java
+++ src/NetworkingServer.java
@@ -5,5 +5,21 @@ import java.io.*;
 public class NetworkingServer {
     public static void main(String[] args) {
 
+        ServerSocket server = null;
+        Socket client;
+
+        //Default port number we are going to use
+        int portnumber = 1234;
+        if(args.length >= 1)
+        {
+            portnumber= Integer.parseInt(args[0]);
+        }
+
+        //Create Server side Socket
+        try{
+            server = new ServerSocket(portnumber);
+        } catch (IOException ie) {
+            System.out.println("Cannot open socket"+ ie);
+        }
     }
 }
