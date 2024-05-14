d1b5ffb6dbe12f81d138cec5b1f30c38ff6006e4
YOHA1505 <you@example.com>
2024-05-14 13:57:09 +0200

    fixa rött

---
+++ src/NetworkingServer.java
@@ -0,0 +1,9 @@
+/*SERVER - May be enhanced to work for multiple clients*/
+import java.net.*;
+import java.io.*;
+
+public class NetworkingServer {
+    public static void main(String[] args) {
+
+    }
+}
